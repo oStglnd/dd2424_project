87d3b6192119e8309c39cf06a5e5f2fcfe36ebf1
oStglnd <stigland@kth.se>
2023-05-24 11:23:36 +0200

    fixed comp and plots

--- param_comp.py
+++ param_comp.py
@@ -31,9 +31,10 @@ for fname in fnames:
             'embeddings':params['embeddings'],
             'embedding_dim':params['embedding_dim'],
             'seq_length':params['seq_length'],
-            'units':params['units'],
-            'trainLoss':np.mean(params['trainLossHist'][-1000:]),
-            'valLoss':np.mean(params['valLossHist'][-1000:])
+            # 'units':params['units'],
+            'layers':len(params['units']),
+            'trainLoss':np.mean(params['trainLossHist'][-50:]) / params['seq_length'],
+            'valLoss':np.mean(params['valLossHist'][-50:]) / params['seq_length']
         }
     
 
@@ -42,33 +43,47 @@ for fname in fnames:
 plotData = pd.DataFrame(dataDict).T
 
 # # get plotVals
-# pDropAcc = plotData.groupby('pDrop').mean()['acc'] * 100
-# mAcc = plotData.groupby('m').mean()['acc'] * 100
+layers = plotData.groupby(['layers', 'embeddings']).mean()['trainLoss']
+layers = layers.reset_index()
 
-# # plot dropout accuracy
-# sns.barplot(
-#     x=pDropAcc.index, 
-#     y=pDropAcc.values, 
-#     palette='magma'
-# )
 
-# plt.ylim(45, 55)
-# plt.xlabel('Dropout rate, $p_{drop}$')
-# plt.ylabel('%')
-# plt.title('Mean accuracy per dropout rates')
-# plt.savefig(plot_path + 'compDropout.png', dpi=400)
-# plt.show()
+g = sns.catplot(
+    data=layers, 
+    kind="bar",
+    x="layers", 
+    y="trainLoss", 
+    hue="embeddings",
+    palette="Blues", 
+    alpha=1.0, 
+    height=6
+)
 
-# # plot m accuracy
-# sns.barplot(
-#     x=mAcc.index.astype('int64'), 
-#     y=mAcc.values, 
-#     palette='viridis'
-# )
+plt.ylim(1.5, 2.5)
+plt.xlabel('Number of layers')
+plt.ylabel('Loss', rotation=0, labelpad=15)
+plt.title('Mean loss for last 5000 steps')
+plt.savefig(plot_path + 'compEmbedd', dpi=200)
+plt.show()
 
-# plt.ylim(45, 55)
-# plt.xlabel('Number of hidden nodes, $m$')
-# plt.ylabel('%')
-# plt.title('Mean accuracy per layer width')
-# plt.savefig(plot_path + 'compLayerM.png', dpi=400)
-# plt.show()
+# # get plotVals
+layers = plotData.groupby(['layers', 'seq_length']).mean()['trainLoss']
+layers = layers.reset_index()
+
+
+g = sns.catplot(
+    data=layers, 
+    kind="bar",
+    x="layers", 
+    y="trainLoss", 
+    hue="seq_length",
+    palette='rocket', 
+    alpha=1.0, 
+    height=6
+)
+
+plt.ylim(1.5, 2.5)
+plt.xlabel('Number of layers')
+plt.ylabel('Loss', rotation=0, labelpad=15)
+plt.title('Mean loss for last 5000 steps')
+plt.savefig(plot_path + 'compSeqLen.png', dpi=200)
+plt.show()
